0b87ca8df19cf2f55642eb6142d99a8f6a6ccfe7
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-11-03 15:26:19 +0700

    submit

---
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { PaymentDetail } from './payment-detail.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+
+  constructor(private http: HttpClient) { }
+
+  readonly baseURL = 'http://localhost:61236/api/PaymentDetail';
+  formData: PaymentDetail = new PaymentDetail();
+  list: PaymentDetail[];
+
+  // tslint:disable-next-line:typedef
+  postPaymentDetail() {
+    return this.http.post(this.baseURL, this.formData);
+  }
+
+  // tslint:disable-next-line:typedef
+  putPaymentDetail() {
+    return this.http.put(`${this.baseURL}/${this.formData.paymentDetailId}`, this.formData);
+  }
+
+  // tslint:disable-next-line:typedef
+  deletePaymentDetail(id: number) {
+    return this.http.delete(`${this.baseURL}/${id}`);
+  }
+
+  // tslint:disable-next-line:typedef
+  refreshList() {
+    this.http.get(this.baseURL)
+      .toPromise()
+      .then(res => this.list = res as PaymentDetail[]);
+  }
+
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+nama 	:muhammad fauzaan irsyaadi
+kode 	:025
+
+backend : 
+PaymentAPISqlite adalah aplikasi backend yang menyediakan API
+ masuk direktori 
+ jalankan dengan IIS
+
+Frontend
+PaymentAPP adalah aplikasi angular yang digunakan untuk antarmuka API
+dengan fitur CRUD
+ masuk direktori
+ jalankan perintah : npm install --save
+ jalankan perintah : ng serve --open	
